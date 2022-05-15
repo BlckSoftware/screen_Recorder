c7b980a1829672fd4fb64e229a7def2ff337cb40
BlckSoftware <huseyinkarayazim@gmail.com>
2022-05-15 17:06:48 +0300

    Screen Recoder

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Captura;
+
+namespace screen_Recorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            int input;
+
+            start:
+            Console.WriteLine("Ekran Kaydını Başlatmak İçin 1'e Basın... \nProgramı Kapatmak İçin 2'ye Basın...\nKare Hızı ve Kalite Belirlemek İçin 3'e Basın... (Opsiyon)");
+            input = Convert.ToInt32(Console.ReadLine());
+           if (input == 3) { 
+                frm: Console.Write("Önerilen Kare Hızı 10'dur \nKare Hızı Belirlemek İçin Bir Değer Girin : "); int frame = Convert.ToInt16(Console.ReadLine());
+              
+                if (frame == null) { Console.Write("Kare Hızı Belirleyin: "); goto frm; }
+               qlt: Console.Write("Önerilen Kalite Değeri 70'dir \nKalite Değeri Belirlemek İçin Bir Değer Girin : "); int quality = Convert.ToInt16(Console.ReadLine());
+               
+                if (quality == null) { Console.Write("Kalite Değeri Belirleyin: "); goto qlt; }
+
+                Console.Write("Kayıt Adı Belirleyin : ");
+
+                string input2 = Console.ReadLine();
+                if (input2 == null || input2 == "" || input2 == " ") { input2 = "isimsiz "; }
+                string filepath = AppDomain.CurrentDomain.BaseDirectory + input2 + "_" + DateTime.Now.Date.ToLongDateString().Replace('/', '_') + ".avi";
+                var rec = new Recorder(new RecorderParams(filepath, frame, SharpAvi.KnownFourCCs.Codecs.MotionJpeg, quality));
+                Console.Clear();
+                Console.WriteLine("Kayıt Başladı ...");
+
+                Console.WriteLine("\nBelirlenen Kayıt Adı: "+input2+"\nBelirlenen Kare Hızı :"+frame+"\nBelirlenen Kalite Değeri :"+quality+"\n");
+                Console.Write("\nKaydı Durdurmak İçi Herhangi Bir Tuşa  Basın ...");
+                Console.ReadKey();
+                rec.Dispose();
+            }
+
+            else if (input >= 4) { Console.WriteLine("Geçersiz Bir Değer Girdiniz.\nGirdiğiniz değer 1 veya 2 olabilir\n\n"); goto start; }
+
+            else if (input == 1)
+            {
+                int frame = 10;int quality = 70;
+                Console.Write("Kayıt Adı Belirleyin : ");
+
+                string input2 = Console.ReadLine();
+                if (input2 == null || input2 == "" || input2 == " ") { input2 = "isimsiz "; }
+                string filepath = AppDomain.CurrentDomain.BaseDirectory + input2 + "_" + DateTime.Now.Date.ToLongDateString().Replace('/', '_') + ".avi";
+                var rec = new Recorder(new RecorderParams(filepath, frame, SharpAvi.KnownFourCCs.Codecs.MotionJpeg, quality));
+
+                Console.WriteLine("Kayıt Başladı ...");
+                Console.Write("\nKaydı Durdurmak İçi Herhangi Bir Tuşa  Basın ...");
+                Console.ReadKey();
+
+                // Finish Writing
+                rec.Dispose();
+            }
+            else if (input == 2) { }
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("screen_Recorder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("screen_Recorder")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("263bab5f-34e7-453d-bad0-18927c1aa33b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
